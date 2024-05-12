b9f7d9c90c1a26535da568acccad62b6720313e9
jeafonso <jeafonso93@gmail.com>
2024-05-12 15:48:26 +0100

    fix: removing unnecessary comments

--- fitness/static/fitness/scripts/schedule_workout.js
+++ fitness/static/fitness/scripts/schedule_workout.js
@@ -104,7 +104,6 @@ function getCookie(name) {
         const cookies = document.cookie.split(';');
         for (let i = 0; i < cookies.length; i++) {
             const cookie = cookies[i].trim();
-            // Check if the cookie name matches the CSRF token cookie name
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
